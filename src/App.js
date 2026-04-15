729115444a1dd418df6d9cd62b5b51a9ebbf1a28
Lakshmi Dubey <lakshmi.mcahbti@gmail.com>
2026-04-15 17:04:59 -0500

    add tasks

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Footer } from "./components/Footer";
 import { TaskList } from "./components/TaskList";
 import './App.css';
 import AddTasks from "./components/AddTasks";
+import { useState } from "react";
 
 function App() {
-
+  const [tasks, setTasks] = useState([
+      
+    ]);
   return (
     <div className="App">
       <Header />
-        <AddTasks />
-      <TaskList />
+        <AddTasks tasks ={tasks} setTasks={setTasks} />
+        <TaskList tasks={tasks} setTasks={setTasks} />
       <Footer />
     </div>
   );
--- src/components/AddTasks.js
+++ src/components/AddTasks.js
@@ -1,14 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
 import './AddTasks.css'
 
-export default function AddTasks() {
+export default function AddTasks({tasks, setTasks}) {
+  const [task,setTask]=useState('');
+  const [status,setStatus]=useState(false);
+
+ const handleInput= (e)=>{
+    setTask(e.target.value);
+  }
+  const handleReset=()=>{
+     setTask('');
+     setStatus(false);
+  }
+ const handleSubmit=(e)=>{
+    
+      e.preventDefault();
+      const newTask={
+        id:Math.floor(Math.random()*1000),
+        name:task,  
+        completed:Boolean(status)
+      }
+      console.log(newTask);
+      setTasks([...tasks,newTask]);
+      handleReset();
+    }
+    const handleStatusChange=(e)=>{
+      setStatus(e.target.value );
+          }
   return (
    <section className='addtask'>
     <h2>Add Tasks</h2>
-    <form>
+    <form onSubmit={handleSubmit}>
     <label htmlFor="taskName">Task Name:</label>
-       <input type="text" name="task" id="task" placeholder='Task Name' autoComplete='off'/>
+        <input onChange={handleInput}  type="text" value={task} name="task" id="task" placeholder='Task Name' autoComplete='off'/>
+        <select onChange={handleStatusChange} value={status}>
+          <option value={false}>Pending</option>
+          <option value={true}>Completed</option>
+         </select>
+      <span onClick={handleReset} className='reset'>Reset</span>
         <button type='submit'>Add Task</button>
+     
+     
     </form>
    </section>
   )
